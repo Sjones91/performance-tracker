14a4e367143448200ecf6b0ea65f1ca24532a3d9
Sean Jones <sean__jones@hotmail.com>
2022-01-27 21:39:33 +0000

    build changes to GetWeather component

--- src/components/header.js
+++ src/components/header.js
@@ -8,6 +8,7 @@ function Header() {
         <div className="header d-f-col">
             <h1 className="headTitle d-f-row ">Employee Performance Tracker</h1>
             <TimeAndDate className="timeDate"></TimeAndDate>
+            <Weather/>
         </div>
     )
 }
--- src/functions/weather.js
+++ src/functions/weather.js
@@ -1,24 +1,24 @@
 import "../App.scss";
-import React, { useState } from 'react';
+import React, {useState} from 'react';
+
 
 function GetWeather(){
     const url = "https://goweather.herokuapp.com/weather/London";
-    const [weather,setWeather] = useState()
+    let weather = "";
     fetch(url)
-     .then((res)=>{
-         res.json()
-     })
-     .then((data)=>{
-         setWeather(data)
-     })
-     .catch((error) => {
-         console.log(error)
-     }
-    )
-    console.log(weather);
-    return(
-        <p>{weather}</p>
-    )
+        .then((res)=> res.json())
+        .then((data)=> {
+            weather = data;
+            
+        })
+        .catch((error)=> {
+            console.log(error)
+        })
+        
+        return (
+            <p>weather</p>
+        )
+
 }
 
 export default GetWeather;
